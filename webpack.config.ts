20b83f5a7244fb58c9b1e66462bbf2ce10af6c79
mugtaba-subahi <m_subahi@hotmail.com>
2020-05-27 08:03:36 +0100

    fixed webpack for development

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,9 @@
 import slsw from 'serverless-webpack';
 
+const mode = process.env.NODE_ENV === ('production' || 'staging') ? 'production' : 'development';
+
 module.exports = {
+  mode, // dev mode allows hot reload
   entry: slsw.lib.entries,
-  optimization: { minimize: true },
+  externals: ['bufferutil', 'utf-8-validate'], // stops warning - why?
 };
